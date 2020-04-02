a593d8c7de718349bb7956b40fbdc734ab5d75df
Adib Faqih <foezst@gmail.com>
2020-04-02 15:52:47 +0700

    release 0

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 "use strict"
 
+const fs = require('fs');
+
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  
+  constructor(id, firstName, lastName, email, phone, createdAt) {
+    this.id = id;
+    this.first_name = firstName;
+    this.last_name = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.created_at = createdAt;
+  }
+
+}
+
+let persons = [];
+let people = fs.readFileSync('people.csv', 'utf8');
+let arrayPeople = people.split('\n');
+
+for (let i = 0; i < arrayPeople.length; i++) {
+  let temp = arrayPeople[i].split(',');
+  persons.push(new Person(temp[0],temp[1],temp[2],temp[3],temp[4],temp[5]));
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = this.peoples(file);
   }
 
   get people() {
     return this._people
+  }
+
+  get file() {
+    return this._file;
+  }
+
+  peoples(file) {
+    let persons = [];
+    let people = fs.readFileSync(file, 'utf8');
+    let arrayPeople = people.split('\n');
+
+    for (let i = 1; i < arrayPeople.length; i++) {
+      let temp = arrayPeople[i].split(',');
+      persons.push(new Person(temp[0],temp[1],temp[2],temp[3],temp[4],temp[5]));
+    }
+    return persons;
   }
 
   addPerson() {}
@@ -23,3 +58,5 @@ class PersonParser {
 let parser = new PersonParser('people.csv')
 
 console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
+
+// console.log(parser);
