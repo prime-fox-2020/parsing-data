f17d495073e773924a9357353b58c40b7c2a3b75
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-02 18:21:37 +0700

    Release 1

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ class Person {
     this._firstName = firstName;
     this._lastName = lastName;
     this._email = email;
+    this._phone = phone;
     this._createdAt = new Date();
   }
 }
@@ -17,18 +18,19 @@ class Person {
 class PersonParser {
 
   constructor(file) {
-    this._file = fs.readFileSync(file, 'utf8');
-    this._people = []
+    this._file = file;
+    this._people = [];
     
-    this._file.split('\r\n').forEach((el, i, arr) => {
+    fs.readFileSync(file, 'utf8').split('\r\n').forEach((el, i, arr) => {
       if (i > 0) {
         let data = el.split(',');
         this._people.push({
-          id: data[0],
-          firstName: data[1],
-          lastName: data[2],
-          email: data[3],
-          createdAt: data[4]
+          _id: data[0],
+          _firstName: data[1],
+          _lastName: data[2],
+          _email: data[3],
+          _phone: data[4],
+          _createdAt: data[5]
         })
       }
     })
@@ -39,12 +41,29 @@ class PersonParser {
     return this._people;
   }
 
-  addPerson() {}
+  get file() {
+    return this._file;
+  }
+
+  addPerson(obj) {
+    this._people.push(obj);
+  }
+
+  save() {
+    let personData = 'id,first_name,last_name,email,phone,created_at';
+    this._people.forEach((el) => {
+      personData += `\r\n${el._id},${el._firstName},${el._lastName},${el._email},${el._createdAt}`;
+      // console.log(`${el._id}, ${el._firstName}, ${el._lastName}, ${el._email}, ${el._createdAt}\r\n`)
+    })
+    fs.writeFileSync(this._file, personData);
+  }
 
 }
 
-let parser = new PersonParser('people.csv')
-let bagon = new Person((parser.people[parser.people.length - 1].id + 1), 'Bagong', 'Jyay', 'bagong@bagongcorp.com', '0888812121')
-console.log(bagon)
-// console.log(parser.people)
-// console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
+let parser = new PersonParser('people.csv');
+console.log(parser.people)
+parser.addPerson(new Person(parseInt(parser.people[parser.people.length - 1]._id) + 1, 'Bagong', 'Jyay', 'bagong@bagongcorp.com', '0888812121'));
+console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`);
+console.log(parser.people[parser.people.length - 1]);
+
+// parser.save();
