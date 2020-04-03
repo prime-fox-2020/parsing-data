1f43091584bf2331b360f3f05912db417c07761b
Sujackson <su_jackson99@yahoo.com>
2020-04-03 22:01:20 +0700

    semua sudah di dalam class personparser

--- index.js
+++ index.js
@@ -2,15 +2,6 @@
 
 const fs =require('fs')
 
-//read
-const data=fs.readFileSync("people.csv","utf8")
-
-
-const arrayOfString=data.split('\n')
-
-let arrayOfObject=[]
-
-
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
@@ -24,18 +15,23 @@ class Person {
   }
 }
 
-for (let i = 1; i < arrayOfString.length; i++) {
-  const buffer=arrayOfString[i].split(',')
-  arrayOfObject.push(new Person(buffer[0],buffer[1],buffer[2],buffer[3],buffer[4],buffer[5]))
-}
-
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = this.read()
   }
 
+  read(){
+    const data=fs.readFileSync("people.csv","utf8")
+    const arrayOfString=data.split('\n')
+    let arrayOfObject=[]
+    for (let i = 1; i < arrayOfString.length; i++) {
+        const buffer=arrayOfString[i].split(',')
+        arrayOfObject.push(new Person(buffer[0],buffer[1],buffer[2],buffer[3],buffer[4],buffer[5]))
+    }
+    return arrayOfObject
+  }
 
   get people() {
     return this._people
@@ -54,13 +50,12 @@ class PersonParser {
   } 
   
   addPerson(id, first_name, last_name, email, phone, created_at= new Date().toISOString()) {
-    let x=new Person(id, first_name, last_name, email, phone, created_at)
-    arrayOfObject.push(x)
+    this._people.push(new Person(id, first_name, last_name, email, phone, created_at))
     return this
   }
   save(){
     let data='id, first_name, last_name, email, phone, created_at'
-    arrayOfObject.forEach(arr => {
+    this._people.forEach(arr => {
       data += `\n${arr.id},${arr.first_name},${arr.last_name},${arr.email},${arr.phone},${arr.createdAt=new Date().toISOString()}`;
     });
     fs.writeFileSync(this._file,data)
@@ -69,8 +64,6 @@ class PersonParser {
 }
 
 let parser = new PersonParser('people.csv')
-parser.people=arrayOfObject
-
 const faker = require('faker')
 const jumlahData = 5
 
@@ -82,9 +75,7 @@ for(let i = 1; i <= jumlahData; i++){
   let phone = faker.phone.phoneNumber()
   let date = faker.date.past().toISOString()
   parser.addPerson(parser.people.length+1, firstName, lastName, email, phone, date)
-
 }
+
 parser.save()
 console.log(`There are ${parser.people.length} people in the file '${parser.file}'.`)
-
-
